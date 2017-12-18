d45ef8dd8d5e255834482e70a88a996b523ed37d
Marina Paskal <paskalm@rogers.com>
2017-12-18 18:05:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,16 @@
 class Song
   attr_accessor :artist, :name
 
-  def initialize(name = nil)
+  def initialize(name)
     @name = name
+  end
+
+  def name=(name = nil)
+    @name = name
+  end
+
+  def name
+    @name
   end
 
   def artist_name
